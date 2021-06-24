44dc1000ab7d7bf1a9ee57162534048c3691a461
kaushikvrudhula <84515709+kaushikvrudhula@users.noreply.github.com>
2021-06-24 19:30:41 +0530

    completd

---
+++ Day-9/d9i.js
@@ -0,0 +1,23 @@
+var Bleach = {
+    firstname: 'Ichigo' ,
+    lastname: 'Kurosaki' ,
+    getFullname: function()
+    {
+        var full_name =this.firstname + ' ' + this.lastname;
+
+        return full_name;
+    }
+};
+
+
+
+var jobdescription = function(shikai , bankai) {
+    console.log(this.getFullname() + ':- shinigami');
+    console.log(this.getFullname() + ' shikai ' + shikai + ' bankai ' + bankai);
+};
+
+jobdescription.call(Bleach,'Zangetsu', 'Tensa Zangetsu'); // Pika Chu  loves sushi and algorithms
+jobdescription.call(Bleach,['Zangetsu', 'Tensa Zangetsu']);
+
+var logPokemon = getFullname.bind(Bleach);
+logPokemon();
